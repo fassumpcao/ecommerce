50cd1e89f4e05ad8d9876cb6dabec02ee8c065f8
fassumpcao <felipe.p.assumpcao@gmail.com>
2019-06-08 00:59:00 +0000

    Site categorias.

--- index.php
+++ index.php
@@ -292,6 +292,18 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues()
+	]);
+});
+
 $app->run();
 
  ?>
