fee0b924cadfa068d682feccdd5a967ba90f8213
fassumpcao <felipe.p.assumpcao@gmail.com>
2019-06-15 00:30:15 +0000

    Site - Meus Pedidos.

--- functions.php
+++ functions.php
@@ -1,11 +1,12 @@
 <?php
 
 use \Hcode\Model\User;
+use \Hcode\Model\Cart;
 
 function formatPrice($vlprice)
 {
     if (! $vlprice > 0) $vlprice = 0;
-    
+
     return number_format($vlprice, 2, ",", ".");
 }
 
@@ -21,4 +22,21 @@ function getUserName()
     return $user->getdesperson();
 }
 
+function getCartNrQtd()
+{
+    $cart = Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+
+    return $totals['nrqtd'];
+}
+
+function getCartVlSubTotal()
+{
+    $cart = Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+
+    return formatPrice($totals['vlprice']);
+}
  ?>
--- site.php
+++ site.php
@@ -216,7 +216,7 @@ $app->post("/checkout", function(){
 
     $cart = Cart::getFromSession();
 
-    $totals = $cart->getCalculateTotal();
+    $cart->getCalculateTotal();
 
     $order = new Order();
 
@@ -227,7 +227,7 @@ $app->post("/checkout", function(){
         'idstatus'=>OrderStatus::EM_ABERTO,
         'vltotal'=>$cart->getvltotal()
     ]);
-//'vltotal'=>$totals['vlprice'] + $cart->getvlfreight()
+
     $order->save();
 
     header("Location: /order/".$order->getidorder());
@@ -452,7 +452,8 @@ $app->get("/boleto/:idorder", function($idorder){
     $dias_de_prazo_para_pagamento = 10;
     $taxa_boleto = 0.00;
     $data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006";
-    $valor_cobrado = $order->getvltotal(); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+    $valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+    $valor_cobrado = str_replace(".", "",$valor_cobrado);
     $valor_cobrado = str_replace(",", ".",$valor_cobrado);
     $valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 
@@ -509,4 +510,38 @@ $app->get("/boleto/:idorder", function($idorder){
     require_once($path . "funcoes_itau.php");
     require_once($path . "layout_itau.php");
 
+});
+
+$app->get("/profile/orders", function(){
+    User::verifyLogin(false);
+
+    $user = User::getFromSession();
+
+    $page = new Page();
+
+    $page->setTpl("profile-orders", [
+        'orders'=>$user->getOrders()
+    ]);
+});
+
+$app->get("/profile/orders/:idorder", function($idorder){
+    User::verifyLogin(false);
+
+    $order = new Order();
+
+    $order->get((int)$idorder);
+
+    $cart = new Cart();
+
+    $cart->get((int)$order->getidcart());
+
+    $cart->getCalculateTotal();
+
+    $page = new Page();
+
+    $page->setTpl("profile-orders-detail", [
+        'order'=>$order->getValues(),
+        'cart'=>$cart->getValues(),
+        'products'=>$cart->getProducts()
+    ]);
 });
